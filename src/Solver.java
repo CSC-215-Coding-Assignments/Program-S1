96c57a0c1dcd5a12bd98f48a46c39b03895a1aa5
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-10 20:30:27 -0500

    Setup

---
+++ src/Solver.java
@@ -0,0 +1,61 @@
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class Solver {
+
+    //HashSet of State
+    //Queue of States (use as List<State>)
+
+    static int[] sizes;
+
+
+    public Solver(int[] sizes) {
+        this.sizes = sizes;
+    }
+
+    public void findSolution(PrintStream out) {
+
+        //While queue is not empty, add to hashset, check if it is a solution, if it is, then go to solutionFound(), otherwise create children
+    }
+
+    public void createChildren(State parent) {
+        //Systematically creating children, checking if they are in hashset, if not, then add to queue
+    }
+
+    public void solutionFound(State state) {
+
+    }
+
+
+    public class State {
+        State parent;
+        int[] cups;
+        int from,to;
+
+        public State(State parent, int from, int to) {
+            this.parent = parent;
+            cups = new int[sizes.length];
+            this.from = from;
+            this.to = to;
+            //Create a state that pours from parent, from form and to to.
+
+
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            State state = (State) o;
+
+            return Arrays.equals(cups, state.cups);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(cups);
+        }
+    }
+}
