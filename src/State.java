1d2b6d9506f524b7fa1658689a48a33cad6b8d02
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-10 20:13:10 -0500

    Added State class

---
+++ src/State.java
@@ -0,0 +1,15 @@
+/**
+ * function: returns true if one of the values is the specified amount
+ */
+public class State {
+
+
+    public boolean hasAmount(int amount) {
+        return false;
+    }
+
+    public State pour(int from, int to) {
+
+        return null;
+    }
+}
