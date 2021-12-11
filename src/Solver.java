744080fa24e0f94321d05643f9560deb231ad808
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-10 20:38:45 -0500

    Children have been added

--- src/Solver.java
+++ src/Solver.java
@@ -2,11 +2,13 @@ import java.io.File;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solver {
 
     HashSet<State> A = new HashSet<>();
+    Queue<State> queue = new LinkedList<>();
     //Queue of States (use as List<State>)
 
     static int[] sizes;
@@ -23,6 +25,17 @@ public class Solver {
 
     public void createChildren(State parent) {
         //Systematically creating children, checking if they are in hashset, if not, then add to queue
+        for(int i = 0; i < sizes.length - 1; i++) {
+            for(int j = i + 1; j < sizes.length; j++) {
+                State a = new State(parent,i,j), b = new State(parent,j,i);
+                if(!A.contains(a)) {
+                    queue.add(a);
+                }
+                if(!A.contains(b)) {
+                    queue.add(b);
+                }
+            }
+        }
     }
 
     public void solutionFound(State state) {
